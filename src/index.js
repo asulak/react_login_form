612b72719546a934f3e865ddb8023c32dedcea49
asulak <123790407+asulak@users.noreply.github.com>
2023-09-24 20:22:13 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,14 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { AuthProvider } from './context/AuthProvider';
+import { BrowserRouter, Routes, Route } from 'react-router-dom;'
 
 # Note AuthProvider encloses the app
 
 ReactDOM.render(
   <React.StrictMode>
+  <BrowserRouter>
     <AuthProvider>
+      <Routes>
+      <Route path="/*" element={<App/>} /> # /* Means we'll have routes nested inside the root 
+    </Routes>
       <App />
     </AuthProvider>
+  </BrowserRouter
   </React.StrictMode>,
   document.getElementById('root')
 );
