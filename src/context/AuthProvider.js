b70f40313450eff1ea3df9b9e4e4d684771330df
asulak <123790407+asulak@users.noreply.github.com>
2023-09-24 19:38:29 -0600

    Update AuthProvider.js
    With comments

--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -2,9 +2,14 @@ import { createContext, useState } from "react";
 
 const AuthContext = createContext({});
 
+# Destructure the children, the children represents the components nested inside AuthProvider
+
 export const AuthProvider = ({ children }) => {
     const [auth, setAuth] = useState({});
 
+#The children are the components nested within AuthProvider. The AuthContext will provide
+    context for index.js .
+    
     return (
         <AuthContext.Provider value={{ auth, setAuth }}>
             {children}
